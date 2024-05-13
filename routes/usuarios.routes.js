b24e64f9df612ff82dd0aa913bcd07699e74beef
Martin Egea <mar_egea@hotmail.com>
2024-05-13 11:44:37 -0300

    Ruta de usuarios

--- routes/usuarios.routes.js
+++ routes/usuarios.routes.js
@@ -20,15 +20,53 @@ router.post('/login', (req, res)=>{
     }
 })
 
+//obtener usuario por ID
 router.get('/byId/:id', (req, res)=>{
     const id = parseInt(req.params.id)
-
     const result = userData.find(e => e.id === id)
 
     if(result){
         res.status(200).json(result)
     }else{
         res.status(400).json(`Usuario con el id: ${id} no se encuentra`)
+    }
+})
+
+//modificar contraseña de usuario por ID
+router.put('/update/:userID', (req,res)=>{
+    const user_id = req.params.userID
+    const new_user_pass = req.body.password
+
+    try{
+        const index = userData.findIndex(e => e.id == user_id)
+        if(index != -1){
+            userData[index].password = new_user_pass
+            writeFile('./data/usuarios.json', JSON.stringify(userData,null,2))
+            res.status(200).json('Contraseña actualizada!')
+        }else{
+            res.status(400).json('no se encontro el usuario')
+        }
+    }catch(error){
+        res.send(500).json(`Error al actualizar la contraseña`)
+    }
+})
+
+//Eliminar usuario por ID
+router.delete('/delete/:userID', (req, res)=>{
+    const user_id = req.params.userID
+    
+    try{
+        const index = userData.findIndex(e => e.id == user_id)
+
+        if(index != -1){
+            userData.splice(index,1)
+            writeFile('./data/usuarios.json', JSON.stringify(userData,null,2))
+            res.status(200).json('usuario eliminado')
+        }else{
+            res.status(400).json('no se encontro el usuario')
+        }
+    }catch(error){
+        res.send(500).json('Error al intentar eliminar un usuario')
     }
 })
 
