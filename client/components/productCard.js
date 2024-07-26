40b05c48546098eb6d7b5dfa9d48de6743f20b76
Martin Egea <mar_egea@hotmail.com>
2024-07-26 15:08:18 -0300

    mejora de style en el ProductCard

--- client/components/productCard.js
+++ client/components/productCard.js
@@ -4,8 +4,8 @@ export const productCard = (id,nombre,desc,precio,imagen)=>{
     return `
     <div class="bg-gray-700 max-w-sm rounded overflow-hidden shadow-xl shadow-slate-950 my-3 mx-2 animate-fade-up">
         <div class="p-5 flex flex-col h-full">
-            <div class="rounded-xl overflow-hidden">
-                <img src="${imagen}" alt="">
+            <div class="rounded-xl overflow-hidden flex items-center justify-center bg-white">
+                <img src="${imagen}" class="rounded-xl max-w-sm max-h-80 min-w-64 min-h-80 object-contain" alt="">
             </div>
             <h5 class="text-sky-200 text-2xl md:text-3xl font-medium mt-3">${nombre}</h5>
             <p class="text-slate-200 text-lg mt-3">${desc}</p>
